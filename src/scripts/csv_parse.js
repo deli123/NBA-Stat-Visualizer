3a040c0eadfb3b28e728aab35b61832e4bc42b3b
Derek <liderek63@gmail.com>
2022-08-16 00:40:02 -0700

    able to read csv

--- src/scripts/csv_parse.js
+++ src/scripts/csv_parse.js
@@ -1,51 +1,32 @@
-const fs = require("fs");
-const { parse } = require("csv-parse");
-const path = require("path");
+const fs = require('fs');
+const Papa = require('papaparse');
+const path = require('path');
 
-const filePath = path.join(__dirname, "../../assets/data/player-data.csv");
-const stream = fs.createReadStream(filePath);
+// const csvFilePath = '../../assets/data/player-data.csv'
+const csvFilePath = path.join(__dirname, "../../assets/data/player-data.csv");
 
-// const data = [];
+// Function to read csv which returns a promise so you can do async / await.
 
-// fs.createReadStream(coolPath)
-//   .pipe(
-//     parse({
-//       delimiter: ",",
-//       columns: true,
-//       ltrim: true,
-//     })
-//   )
-//   .on("data", function (row) {
-//     // push the object row into the array
-//     data.push(row);
-//   })
-//   .on("error", function (error) {
-//     console.log(error.message);
-//   })
-// .on("end", res => {
-//   res.send(Buffer.concat(chunks));
+let data;
+const readCSV = async (filePath) => {
+  const csvFile = fs.readFileSync(filePath)
+  const csvData = csvFile.toString()  
+  return new Promise(resolve => {
+    Papa.parse(csvData, {
+      header: true,
+      complete: results => {
+        console.log('Complete', results.data.length, 'records.'); 
+        resolve(results.data);
+      }
+    });
+  });
+};
 
-// }
-// //   // log the result array
-// //   // console.log("parsed csv data:");
-// //   // console.log(data.length);
-// //   // console.log(data[data.length - 1].NAME);
-// );
+const test = async () => {
+  let parsedData = await readCSV(csvFilePath); 
+  console.log(parsedData);
 
-
-
-function streamToString (stream) {
-  const chunks = [];
-  return new Promise((resolve, reject) => {
-    stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)));
-    stream.on('error', (err) => reject(err));
-    stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')));
-  })
+  for
 }
 
-const getResult = async (stream) => {
-  const result = await streamToString(stream)
-  console.log(result);
-  // return result;
-}
-const data = getResult(stream);
+test();
